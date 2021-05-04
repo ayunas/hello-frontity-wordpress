43e519a6864c366eb9cb23fafcc01d11e75d2c6e
Amir Yunas <ayunas@gmail.com>
2021-05-04 12:44:33 -0500

    created base nav bar with simple links

---
+++ packages/amirs-frontity-theme/src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from "@frontity/components/link";
+
+function NavBar() {
+
+    return (
+        <>
+            <Link link="/">Home</Link>
+            <Link link="/community">Community</Link>
+            <Link link="/contact">Contact</Link>
+            <Link link="/about">About</Link>
+        </>
+    )
+
+
+
+
+}
+
+export default NavBar;
--- packages/amirs-frontity-theme/src/components/Root.jsx
+++ packages/amirs-frontity-theme/src/components/Root.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import {connect} from 'frontity';
+import Link from '@frontity/components/link';
+import NavBar from './NavBar';
 
 
 function Root(props) {
 
     const {state} = props;
-    console.log(state);
 
     return (
         <>
           {/* You can edit your package in:
           <pre>packages/amirs-frontity-theme/src/index.js</pre> */}
+          <NavBar/>
           <h1>Amir's Frontity Theme</h1>
           <p>Current URL : {state.router.link}</p>
+          
         </>
       );
 }
