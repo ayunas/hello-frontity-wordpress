8ebc5b4826c5595a6aadf4ce87d2d9f8df8c9ce3
Amir Yunas <ayunas@gmail.com>
2021-05-04 12:25:35 -0500

    pulled from the Frontity state by connecting Root using connect HoC then grabbing props.state.router.link to show the current url of the browser

--- packages/amirs-frontity-theme/src/components/Root.jsx
+++ packages/amirs-frontity-theme/src/components/Root.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import {connect} from 'frontity';
 
-function Root() {
+
+function Root(props) {
+
+    const {state} = props;
+    console.log(state);
+
     return (
         <>
           {/* You can edit your package in:
           <pre>packages/amirs-frontity-theme/src/index.js</pre> */}
           <h1>Amir's Frontity Theme</h1>
+          <p>Current URL : {state.router.link}</p>
         </>
       );
 }
 
-export default Root;
+export default connect(Root); //HoC to provide state to the component
